efe0836c7e5cc5292f200fc96d91c33c807a17d6
MarcDagher <marcdagher47@gmail.com>
2023-12-17 15:03:46 +0200

    basic login

--- survey-backend/controllers/authController.js
+++ survey-backend/controllers/authController.js
@@ -1,9 +1,9 @@
 const User = require("../models/user.model")
+const jwt = require("jsonwebtoken")
 const bcrypt = require ("bcrypt")
 
 const register = async (request, response) => {
   const {first_name, last_name, email, password, birthday} = request.body
-  console.log(request.body)
   if (!first_name | !last_name | !email | !password | !birthday){
     return response.status(400).send({message: "All fields are required!"})
   } 
@@ -20,4 +20,26 @@ const register = async (request, response) => {
   }
 }
 
-module.exports = { register }
+
+const login = async (request, response) => {
+  const { email, password } = request.body
+
+  // check if user is availabke in DB
+  const user = await User.findOne({ email })
+  if (!user) return response.status(400).send({ message: "Invalid username/password" })
+
+  // check if password is correct
+  const isValidPassword = await bcrypt.compare(password, user.password)
+  if (!isValidPassword) return response.status(400).send({ message: "Invalid username/password" })
+
+  const { password: hashedPassword, _id, ...userDatails } = user.toJSON()
+
+  // generate JWT token
+  const token = jwt.sign({ ...userDatails }, 'secret_key') // payload, secret, optional: algorithm, expiresIn
+  response.status(200).send({
+    user: userDatails,
+    token
+  })
+}
+
+module.exports = { register, login }
