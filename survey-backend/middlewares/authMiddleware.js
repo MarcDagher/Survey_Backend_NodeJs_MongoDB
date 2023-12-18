414123a1838c22fc52f4a13d858265d9fa0a4d9b
MarcDagher <marcdagher47@gmail.com>
2023-12-18 20:55:04 +0200

    Adjust middleware

--- survey-backend/middlewares/authMiddleware.js
+++ survey-backend/middlewares/authMiddleware.js
@@ -1,5 +1,7 @@
 const jwt = require("jsonwebtoken")
 const User = require("../models/user.model")
+const {Role} = require("../models/role.model")
+const { response } = require("express")
 
 const authMiddleware = async (req, res, next) => {
   const token = req.headers["authorization"] ?.split(" ")[1]
@@ -7,11 +9,22 @@ const authMiddleware = async (req, res, next) => {
   if (!token) {
     res.status(403).send("Forbidden")
   } else {
-    const decoded = jwt.verify(token, "secret_key")
-    const user = await User.findOne({email: decoded.email}).select("-password")
-    req.user = user
-    next()
-  }
+    try {
+      const decoded = jwt.verify(token, "secret_key")
+      const roleID = decoded.role_id
+      const role = await Role.findOne({_id:roleID,  role: "admin"})
+      
+      if (!role){
+        res.status(403).send("Forbidden - Not an admin")
+      } else{
+        const user = await User.findOne({email: decoded.email}).select("-password")
+        req.user = user
+        next()
+      }
+    } catch (e) {
+      res.status(500).send("Error in middleware try block")
+    }
+  }  
 }
 
 module.exports = {
