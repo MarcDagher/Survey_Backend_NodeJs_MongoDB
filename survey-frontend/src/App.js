e97b3610243a22888b057b79a69ea08b9f19385f
MarcDagher <marcdagher47@gmail.com>
2023-12-20 09:57:04 +0200

    Initialize survey

--- survey-frontend/src/App.js
+++ survey-frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import LogIn from './pages/LogIn';
 import Register from './pages/Register';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Admin from './pages/admin';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/register'  element={<Register />}/>
           <Route path='/login' element={<LogIn />}/>
+          <Route path='/admin' element={<Admin />} />
         </Routes>
     </BrowserRouter>
     </>
